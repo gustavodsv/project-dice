610fb9612923c522a7c1d5a8c49c8fbdc8523c38
Gustavo <gustavo-dev@hotmail.com>
2020-11-24 12:36:42 -0300

    :tada: Modifiação mo script, para ir de 1 à 6. Adição de alimação alert, caso os numeros seja iguais.

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,12 @@ function roll() {
 }
 function reset(){
 
-    var r1 = Math.floor(Math.random() * 7 );
+    var r1 = Math.floor(Math.random() * (7 - 1) + 1 );
     document.getElementById("dice1").innerHTML = r1;
-    var r2 = Math.floor(Math.random() * 7 );
+    var r2 = Math.floor(Math.random() * (7 - 1) + 1 );
     document.getElementById("dice2").innerHTML = r2;
+
+    if (r1 == r2) {
+        alert("Parabéns. Você conseguiu!");
+    }
 }
